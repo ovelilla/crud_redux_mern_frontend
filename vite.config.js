0827bdc0c6c2e0f81c56846b7628b56e3cb3e671
Oscar <o.velillaa@gmail.com>
2022-07-29 04:01:38 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-    server: {
-        host: "localhost",
-        port: 3000,
-    },
     resolve: {
         alias: [
             { find: "@", replacement: path.resolve(__dirname, "./src") },
